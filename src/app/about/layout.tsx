9620c035dfb466ef6322a95d72f8893b4bc13fe1
Oliverknowledge <123594997+Oliverknowledge@users.noreply.github.com>
2024-08-19 18:20:32 +0100

    fix bug

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter as FontSans} from "next/font/google"
+
+import "../globals.css";
+import NavBar from "@/components/NavBar";
+
+
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: "Buy Me a Coffee",
+  description: "Customers can support their favourite creators by donating coffees (donations) to them.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+   return (
+    <html lang="en">
+      <body className={fontSans.className}>
+        <main>
+        <NavBar/>
+        
+        {children}
+        </main></body>
+    </html>
+  );
+}
